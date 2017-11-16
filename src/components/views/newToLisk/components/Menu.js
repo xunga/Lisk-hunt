0ba67905f4cda171aa40f91a6f7a374b7fff91f2
Tom Ploem <tom.ploem@gmail.com>
2017-11-16 22:29:39 +0100

    awesome picture to show where you at

--- src/components/views/newToLisk/components/Menu.js
+++ src/components/views/newToLisk/components/Menu.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
 
+import support_image from "../img/info.png";
+
 class Menu extends Component {
 
     render(){
         return (
             <div className="newtolisk--container">
-                <div className="support-menu">
+                <div className="newtolisk--info-image">
+                    <img src={support_image} />
+                </div>
+                {/*<div className="support-menu">
                     <div className="content-label-wrap support-wrap">
                         <div className="content-label">SUPPORT</div>
                     </div>
                     <ul>
                         <li><a>New to lisk</a></li>
                     </ul>
-                </div>
+                </div>*/}
             </div>
 
         );
